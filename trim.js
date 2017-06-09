551929dd943a005a6a6fddb6c415a41200a9fb51
Kevin Grondin <kevingrondin@outlook.com>
2017-06-09 11:48:58 +0400

    Create trim.js

---
+++ trim.js
@@ -0,0 +1,23 @@
+/**
+* Trim() retourne une chaine de caractère sans espace
+* @param <String> sString
+* @return <String> sString
+*/
+
+function Trim(sString) {
+    while (sString.substring(0,1) == ' ' || sString.substring(0,1) == '\t' ||
+      sString.substring(0,1) == '\r' || sString.substring(0,1) == '\n')
+    {
+        sString = sString.substring(1, sString.length)
+    }
+    while (sString.substring(sString.length-1, sString.length) == ' ' ||
+      sString.substring(sString.length-1, sString.length) == '\t' ||
+      sString.substring(sString.length-1, sString.length) == '\r' ||
+      sString.substring(sString.length-1, sString.length) == '\n')
+    {
+        sString = sString.substring(0,sString.length-1)
+    }
+    return sString
+}
+
+module.exports.Trim = Trim
