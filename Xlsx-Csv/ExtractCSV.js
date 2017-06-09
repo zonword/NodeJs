2cc34edd0a74f70f10e33b61342b55b1e0263778
Kevin Grondin <kevingrondin@outlook.com>
2017-06-09 11:43:55 +0400

    Create ExtractCSV.js

---
+++ Xlsx-Csv/ExtractCSV.js
@@ -0,0 +1,22 @@
+const parse = require('csv-parse')
+
+var csvData = []
+var data    = null
+var res     = []
+
+function main(fichier){
+  fs.createReadStream(fichier)
+    .pipe(parse({delimiter: ':' }))
+    .on('data', function(csvrow) {
+      csvData.push(csvrow);
+    })
+    .on('end', function() {
+      for(var i = 0; i < csvData.length; i++){
+        data = csvData[i][0].split(';')
+        res.push(data)
+      }
+      return res
+    })
+}
+
+module.exports.ExtractCsv = main
