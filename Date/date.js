e28052843d52a0594df482272f1124ca5d5ac82d
Kevin Grondin <kevingrondin@outlook.com>
2017-06-09 10:35:19 +0400

    Create date.js

---
+++ Date/date.js
@@ -0,0 +1,34 @@
+var _date = new Date();
+_date     = new Date(_date.getTime() + new Date().getTimezoneOffset() * 60 * 1000);
+
+function GetYear(){
+    return _date.getFullYear()
+}
+
+function GetMount(){
+    var month = _date.getMonth() + 1
+    if(month < 10){ return `0${month}` }
+    return month
+}
+
+function Date(){
+    return _date.getDate();
+}
+
+function GetHour(){
+    var hour = _date.getHours();
+    if(hour < 10){ return `0${hour}` }
+    return hour
+}
+
+function GetMinute(){
+    var minute = _date.getMinutes();
+    if(minute < 10){ return `0${minute}` }
+    return minute
+}
+
+module.exports.GetYear   = GetYear;
+module.exports.GetMount  = GetMount;
+module.exports.Date      = GetMount;
+module.exports.GetHour   = GetHour;
+module.exports.GetMinute = GetMinute;
