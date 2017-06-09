b237757f5123edaeabbca04d9f390910bcf3d881
Kevin Grondin <kevingrondin@outlook.com>
2017-06-09 17:36:26 +0400

    Create GetFilesInDir.js

--- DirAndFile/GetFilesInDir.js
+++ DirAndFile/GetFilesInDir.js
@@ -1,32 +1,19 @@
-var _dir 	= null
-var _file 	= null 
+const _GetFileInDir = require('./GetFileInDir')
 
-function searchfiles(directory){
-  _dir = fs.readdirSync(directory)
-  
-  for(var i = 0; i<_dir.length; i++){
-    _file          = _dir[i]
-    existfile     = existsSync(path.join(directory,file))
-    existescape   = existsEscapeSync(file)
-    taillefichier = filesizeSync(path.join(directory,file))
-    if(existfile && taillefichier < 10000000 && !existescape){
-      var extension             = path.extname(file)
-      var deuxpremiercaractere  = String(file).substr(0,2)
-      var file                  = path.join(directory,file)
-      var existfile             = checkfile(filesofdir[i])
-      if(deuxpremiercaractere != "~$" && (extension == ".xlsx" || extension == ".csv") && existfile){
-        try{
-          var fd = fs.openSync(file, 'r+')
-          fs.closeSync(fd)
-          main(false)
-          files.push(new QueueWorker(filesofdir[i],file))
-          metier()
-        }catch(err){
-          if (err.code == "EBUSY") debug(printmauve("un fichier EBUSY"))
-        }
-      }
-    } else if (taillefichier > 10000000) {
-      debug(printmauve(`${file} est trop lourd`))
+var _dir, _file = null 
+const _files    = []
+
+function Main(directory){
+  return new Promise( (resolve,reject) => {
+    _dir = fs.readdirSync(directory)
+
+    for(var i = 0; i<_dir.length; i++){
+      _file = _dir[i]
+      _GetFileInDir(directory,_file)
+        .then(file => { _files.push(file) })
+        .catch(err => { reject(err) })
     }
-  }
+
+    resolve(_files)
+  })
 }
