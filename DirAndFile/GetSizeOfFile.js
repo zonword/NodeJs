689f1e941d6d2c969840f40e9458f979aed03eed
Kevin Grondin <kevingrondin@outlook.com>
2017-06-10 16:35:29 +0400

    Create GetSizeOfFile.js

---
+++ DirAndFile/GetSizeOfFile.js
@@ -0,0 +1,13 @@
+const _stats, _size = null
+
+function Main(file){
+  return new Promise( (resolve,reject) => {
+    try {
+      _stats  = fs.statSync(file)
+      _size    = stats["size"] 
+      resolve(_size)
+    } catch(err) { reject(false) }
+  })
+}
+
+module.exports.GetSizeOfFile = Main
