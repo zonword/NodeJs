9a8e6639a945f24909838e8f0bc6d6eee1d732bb
zonword <kevingrondin@outlook.com>
2018-02-19 11:23:41 +0400

    Create ClassQueueWorker.js

---
+++ ETL-EAI/v2/lib/Class/ClassQueueWorker.js
@@ -0,0 +1,29 @@
+function QueueWorker(name){
+  this.name     = name
+  this.status   = console.draft()
+  this.working  = false
+}
+
+QueueWorker.prototype.GetName     = function() { return this.name }
+QueueWorker.prototype.GetWorking  = function() { return this.working }
+QueueWorker.prototype.SetWorking  = function(b) {
+  var self     = this
+  self.working = b
+  if (b == true) { self.status(printjaune(`[ ${self.name} ]`)) }
+  if (b == false) { self.status(printvert(`[ ${self.name} ]`)) }
+}
+QueueWorker.prototype.SetStatus   = function(message) {
+  var self = this
+  self.status(printmauve(message))
+}
+QueueWorker.prototype.Finish      = function() {
+  var self      = this
+  self.working  = false
+  self.status(printrouge(`[ ${self.name} ]`))
+}
+QueueWorker.prototype.Affichage   = function() {
+  var self = this
+  self.status(printvert(`[ ${self.name} ]`))
+}
+
+module.exports = QueueWorker
